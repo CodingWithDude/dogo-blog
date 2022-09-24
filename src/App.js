5e7ed121778f0eadfeea9309961829daaf73bdf6
CodingWithDude <codingwithdude@gmail.com>
2022-09-24 16:57:20 -0700

    Added abort controller to useFetch
    updated error handling for useFetch and abortCont
    added exact path for home route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Create from "./Components/Crate";
+import Create from "./Components/Create";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <Navbar></Navbar>
+        <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route exact path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
           </Routes>
         </div>
