14907bcf6f0056aa80eb03e0794c3c6191cdd702
CodingWithDude <codingwithdude@gmail.com>
2022-09-25 13:08:55 -0700

    Added delete button to BlogDetails
    Updated BlogDetails with handleClick for delete button

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Mario");
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+    });
+
+    navigate("/");
+  };
 
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -27,7 +45,8 @@ const Create = () => {
           <option value="Mario">Mario</option>
           <option value="Yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog</button>}
       </form>
       <p>{title}</p>
       <p>{body}</p>
