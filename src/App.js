0ca5fc32c3931d3f2b732f958133abe9a81580d1
CodingWithDude <codingwithdude@gmail.com>
2022-09-25 13:13:59 -0700

    Added 404 page and routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Components/Create";
 import BlogDetails from "./Components/BlogDetails";
+import NotFound from "./Components/NotFound";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
             <Route exact path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
             <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
